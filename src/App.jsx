cea44959845b93504b489f3f57618db099be5402
unknown <jhon.osuna08@gmail.com>
2026-06-15 18:25:59 -0500

    feat: GRN-43 Bade de tawilind y React para todos

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import NotFound from './pages/NotFound'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
+
+export default App
