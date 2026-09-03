6efabb54e9c25ba6f7e4722d32b2a2b989dc3a4e
Bohorquez <breynerbohoquez@gmail.com>
2026-09-02 23:53:42 -0500

    feat(GRN-58): agregar rutas de cotizaciones al router

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ import RutaProtegida from './components/RutaProtegida'
 import RutaProtegidaCliente from './components/RutaProtegidaCliente'
 import NotFound from './pages/NotFound'
 import DashboardLayout from './layouts/DashboardLayout'
+import EmpleadoLayout from './layouts/EmpleadoLayout'
+import ControlEmpleado from './pages/ControlEmpleado'
+import ControlLotes from './pages/ControlLotes'
+import CosechasPlaneadas from './pages/CosechasPlaneadas'
 import DashboardHome from './pages/DashboardHome'
-import Users from './pages/Users'
+import Empleados from './pages/Empleados'
 import { Toaster } from 'react-hot-toast'
 import ResetPassword from './pages/Reset-Password'
 import OlvidePassword from './pages/OlvidePassword'
@@ -16,7 +20,6 @@ import Landing from './pages/Landing'
 import OlvidePasswordAdmin from './pages/OlvidePasswordAdmin'
 import ResetPasswordAdmin from './pages/ResetPasswordAdmin'
 import AlertasDeStock from './pages/AlertasDeStock'
-import AnalisisClientes from './pages/AnalisisClientes'
 import ControlStock from './pages/ControlStock'
 import Envios from './pages/Envios'
 import GestionPedidos from './pages/GestionPedidos'
@@ -24,6 +27,12 @@ import RegistroDeVentas from './pages/RegistroDeVentas'
 import ReportesVentas from './pages/ReportesVentas'
 import Transportadoras from './pages/Transportadoras'
 import Catalogo from './pages/Catalogo'
+import Favoritos from './pages/Favoritos'
+import SimuladorCompra from './pages/SimuladorCompra'
+import Foros from './pages/Foros'
+import RegistroEmpresa from './pages/RegistroEmpresa'
+import PromocionesAdmin from './pages/PromocionesAdmin'
+import ResenasAdmin from './pages/ResenasAdmin'
 import VerificarCuenta from './pages/VerificarCuenta'
 import ClienteLayout from './layouts/ClienteLayout'
 import ClienteInicio from './pages/ClienteInicio'
@@ -34,6 +43,10 @@ import CarritoPage from './pages/CarritoPage'
 import CotizacionPage from './pages/CotizacionPage'
 import ConfigurarPedidoPage from './pages/ConfigurarPedidoPage'
 import EstadoPedidoPage from './pages/EstadoPedidoPage'
+import TrazabilidadLotePage from './pages/TrazabilidadLotePage'
+import ComparacionPage from './pages/ComparacionPage'
+import Empresas from './pages/Empresas'
+import MisCotizaciones from './pages/MisCotizacionesPage'
 
 function App() {
   return (
@@ -64,6 +77,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/registro-empresa" element={<RegistroEmpresa />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
         <Route path="/control-interno" element={<AdminLogin />} />
         <Route path="/reset-password" element={<ResetPassword />} />
@@ -79,35 +93,58 @@ function App() {
         }>
           <Route index element={<ClienteInicio />} />
           <Route path="catalogo" element={<Catalogo />} />
+          <Route path="favoritos" element={<Favoritos />} />
+          <Route path="simulador" element={<SimuladorCompra />} />
+          <Route path="foros" element={<Foros />} />
           <Route path="pedidos" element={<MisPedidos />} />
+          <Route path='cotizaciones' element ={<MisCotizaciones/>}/>
+          <Route path='cotizaciones/:id' element = {<CotizacionPage/>}/>
           <Route path="pedidos/:id" element={<EstadoPedidoPage />} />
           <Route path="promociones" element={<Promociones />} />
           <Route path="cuenta" element={<MiCuenta />} />
           <Route path="carrito" element={<CarritoPage />} />
           <Route path="cotizacion" element={<CotizacionPage />} />
           <Route path="configurar-pedido" element={<ConfigurarPedidoPage />} />
+          <Route path="trazabilidad/:id" element={<TrazabilidadLotePage />} />
+          <Route path="comparar" element={<ComparacionPage />} />
+          <Route path="empresas" element={<Empresas />} />
         </Route>
 
         {/* Compatibilidad: cualquier link viejo a /catalogo cae en la vista nueva */}
         <Route path="/catalogo" element={<Navigate to="/cliente/catalogo" replace />} />
 
+        <Route path="/panel-empleado" element={
+          <RutaProtegida rolesPermitidos={["empleado"]}>
+            <EmpleadoLayout />
+          </RutaProtegida>
+        }>
+          <Route index element={<ControlEmpleado />} />
+          <Route path="cosechas" element={<CosechasPlaneadas />} />
+          <Route path="lotes" element={<ControlLotes />} />
+          <Route path="pedidos" element={<GestionPedidos />} />
+          <Route path="ventas" element={<RegistroDeVentas />} />
+          <Route path="transportadoras" element={<Transportadoras />} />
+          <Route path="envios" element={<Envios />} />
+        </Route>
+
         <Route path="/dashboard" element={
-          <RutaProtegida>
+          <RutaProtegida rolesPermitidos={["admin"]}>
             <DashboardLayout />
           </RutaProtegida>
         }>
           <Route index element={<DashboardHome />} />
-          <Route path="usuarios" element={<Users />} />
+          <Route path="empleados" element={<Empleados />} />
 
           {/* Ventas */}
           <Route path="ventas" element={<RegistroDeVentas />} />
           <Route path="reportes" element={<ReportesVentas />} />
-          <Route path="clientes" element={<AnalisisClientes />} />
 
           {/* Pedidos y envíos */}
           <Route path="pedidos" element={<GestionPedidos />} />
           <Route path="envios" element={<Envios />} />
           <Route path="transportadoras" element={<Transportadoras />} />
+          <Route path="promociones" element={<PromocionesAdmin />} />
+          <Route path="resenas" element={<ResenasAdmin />} />
 
           {/* Inventario */}
           <Route path="inventario" element={<ControlStock />} />
